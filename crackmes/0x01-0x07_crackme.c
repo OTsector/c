eae215adbf7570b6d9b8da5d12fb467e4092262f
OT <imot@protonmail.com>
2021-02-11 14:08:23 +0400

    Add files via upload

--- crackmes/0x01-0x07_crackme.c
+++ crackmes/0x01-0x07_crackme.c
@@ -18,57 +18,57 @@
 #include <string.h>
 
 void output(int end) {
-	const char *g="Access Granted";
-	const char *b="Access Denied";
-	if(end==0) {
-		puts(g); exit(0);
-	} else {
-		puts(b); exit(1);
-	}
+  const char *g="Access Granted";
+  const char *b="Access Denied";
+  if(end==0) {
+    puts(g); exit(0);
+  } else {
+    puts(b); exit(1);
+  }
 }
 
 int checker(char *input, char *passwd, int status) {
-	if(status == 2) {
-		if(strcmp(input, passwd) == 0)
-			return 0;
-		else
-			return 1;
-	} else {
-		if(strcmp(input, passwd) == 0)
-			return 2;
-		else return 1;
-		}
+  if(status == 2) {
+    if(strcmp(input, passwd) == 0)
+      return 0;
+    else
+      return 1;
+  } else {
+    if(strcmp(input, passwd) == 0)
+      return 2;
+    else return 1;
+    }
 }
 
 void checkerPlatform(char *input, char *passwd) {
-	output(checker(input, passwd, checker(input, passwd, 3)));
+  output(checker(input, passwd, checker(input, passwd, 3)));
 }
 
 int main(int argc, char *argv[]) {
 
-	const char *data="grihf@ise!njewo";
-//			            |||||||||||||||
-//			            000000000011111
-//			            012345678901234
+  const char *data="grihf@ise!njewo";
+//                  |||||||||||||||
+//                  000000000011111
+//                  012345678901234
 
-	int parser[]={13, 1, 14, 10, 0};
-//	              |   |  |   |   |
-//	              w   r  o   n   g
+  int parser[]={13, 1, 14, 10, 0};
+//              |   |  |   |   |
+//              w   r  o   n   g
 
-	char *passwd=(char*)malloc(sizeof(char)*sizeof(parser)/sizeof(int));
-	for(int a=0; a<sizeof(parser)/sizeof(int); a++) {
-		for(int b=0; b<strlen(data); b++) {
-			if(b == parser[a]) {
-				*(passwd+a)=*(data+b);
-			}
-		}
-	}
+  char *passwd=(char*)malloc(sizeof(char)*sizeof(parser)/sizeof(int));
+  for(int a=0; a<sizeof(parser)/sizeof(int); a++) {
+    for(int b=0; b<strlen(data); b++) {
+      if(b == parser[a]) {
+        *(passwd+a)=*(data+b);
+      }
+    }
+  }
 
-	if(argc<2) {
-		puts("input a argument dude!"); return 1;
-	}
+  if(argc<2) {
+    puts("input a argument dude!"); return 1;
+  }
 
-	checkerPlatform(argv[1], passwd);
+  checkerPlatform(argv[1], passwd);
 
-	return 0;
+  return 0;
 }
